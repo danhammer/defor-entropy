a26ec5365e0131877d86e9a976ca18505e64ea0f
Dan Hammer <dan.s.hammer@gmail.com>
2012-11-15 11:45:15 -0800

    finished entropy functions

--- src/r/consolidate-data.R
+++ src/r/consolidate-data.R
@@ -14,9 +14,31 @@ prep.data <- function(out.name, force = FALSE) {
   }
 }
 
+entropy <- function(p.coll) {
+  ## Return the entropy measure in bits of the probability collection
+  ## in p.coll
+  -1 * sum(p.coll * log2(p.coll))
+}
+
+shannon.entropy <- function(coll, probs = FALSE, normalize = TRUE) {
+  ## A measure of dipsersion or entropy is an increasing function of
+  ## the shannon entropy measure.
+  if (!probs) {
+    T <- sum(coll)
+    coll <- coll / T
+  }
+  H <- entropy(coll)
+  if (normalize) {
+    N <- length(coll)
+    I <- entropy(rep(1/N), N))
+    return(H/I)
+  }
+  else {
+    return(H)
+  }
+}
+
+
 out.code <- prep.data()
-
-
-
 
 
